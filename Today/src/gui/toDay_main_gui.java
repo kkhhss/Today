394b9b7ae3f364c02f6bca89ea65850ef0e53b43
HanSol Kim <yg91238405@gmail.com>
2023-11-09 14:35:29 +0900

    버튼 눌러서 그에 해당하는 메뉴 랜덤 출력 완성

--- Today/src/gui/toDay_main_gui.java
+++ Today/src/gui/toDay_main_gui.java
@@ -1,19 +1,26 @@
 package gui;
 
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class toDay_main_gui extends JFrame{
 	public startP startP = null;
 	public startP selectP = null;
+    public int category = -1;
+    public int menuType = -1;
+    public int temperature = -1;
 	public void startframe() {
 		getContentPane().add(startP);
 	}
@@ -97,7 +104,7 @@ class selectP1 extends JPanel {
     ImageIcon btn1_4 = new ImageIcon("./Button_Image/btn1_4.png");
     ImageIcon btn1_4_1 = new ImageIcon("./Button_Image/btn1_4_1.png");
      
-    public selectP1(toDay_main_gui 	main) {
+    public selectP1(toDay_main_gui main) {
         this.main = main;
         setLayout(null);
          
@@ -110,7 +117,7 @@ class selectP1 extends JPanel {
         korean.setVisible(true);
         add(korean);
         
-        western  = new JButton(btn1_2);
+        western = new JButton(btn1_2);
         western.setRolloverIcon(btn1_2_1);
         western.setBorderPainted(false);
         western.setSize(130, 70);
@@ -140,39 +147,42 @@ class selectP1 extends JPanel {
         korean.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                // 추가: SelectP1 패널을 화면에 추가
-            	setVisible(false);
-                main.getContentPane().add(new selectP2(main));
+                // 추가: SelectP2 패널을 화면에 추가
+                setVisible(false);
+                main.getContentPane().add(new selectP2(main, 0)); // 0은 한식 카테고리를 나타냅니다.
                 main.revalidate();
                 main.repaint();
             }
         });
+
         western.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                // 추가: SelectP1 패널을 화면에 추가
-            	setVisible(false);
-                main.getContentPane().add(new selectP2(main));
+                // 추가: SelectP2 패널을 화면에 추가
+                setVisible(false);
+                main.getContentPane().add(new selectP2(main, 1)); // 1은 양식 카테고리를 나타냅니다.
                 main.revalidate();
                 main.repaint();
             }
         });
+
         chinese.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                // 추가: SelectP1 패널을 화면에 추가
-            	setVisible(false);
-                main.getContentPane().add(new selectP2(main));
+                // 추가: SelectP2 패널을 화면에 추가
+                setVisible(false);
+                main.getContentPane().add(new selectP2(main, 2)); // 2은 중식 카테고리를 나타냅니다.
                 main.revalidate();
                 main.repaint();
             }
         });
+
         japanese.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                // 추가: SelectP1 패널을 화면에 추가
-            	setVisible(false);
-                main.getContentPane().add(new selectP2(main));
+                // 추가: SelectP2 패널을 화면에 추가
+                setVisible(false);
+                main.getContentPane().add(new selectP2(main, 3)); // 3은 일식 카테고리를 나타냅니다.
                 main.revalidate();
                 main.repaint();
             }
@@ -192,7 +202,7 @@ class selectP1 extends JPanel {
 	    ImageIcon btn1_3 = new ImageIcon("./Button_Image/btn2_3.png");
 	    ImageIcon btn1_3_1 = new ImageIcon("./Button_Image/btn2_3_1.png");
 	 
-	 public selectP2(toDay_main_gui main) {
+	 public selectP2(toDay_main_gui main, int category) {
 	        this.main = main;
 	        setLayout(null);
 	         
@@ -226,33 +236,36 @@ class selectP1 extends JPanel {
 	        rice.addActionListener(new ActionListener() {
 	            @Override
 	            public void actionPerformed(ActionEvent e) {
-	                // 추가: SelectP1 패널을 화면에 추가
-	            	setVisible(false);
-	                main.getContentPane().add(new selectP3(main));
+	                // 추가: SelectP3 패널을 화면에 추가
+	                setVisible(false);
+	                main.getContentPane().add(new selectP3(main, category, 0)); // 0은 밥 메뉴를 나타냅니다.
 	                main.revalidate();
 	                main.repaint();
 	            }
 	        });
+
 	        bread.addActionListener(new ActionListener() {
 	            @Override
 	            public void actionPerformed(ActionEvent e) {
-	                // 추가: SelectP1 패널을 화면에 추가
-	            	setVisible(false);
-	                main.getContentPane().add(new selectP3(main));
+	                // 추가: SelectP3 패널을 화면에 추가
+	                setVisible(false);
+	                main.getContentPane().add(new selectP3(main, category, 1)); // 1은 빵 메뉴를 나타냅니다.
 	                main.revalidate();
 	                main.repaint();
 	            }
 	        });
+
 	        noodle.addActionListener(new ActionListener() {
 	            @Override
 	            public void actionPerformed(ActionEvent e) {
-	                // 추가: SelectP1 패널을 화면에 추가
-	            	setVisible(false);
-	                main.getContentPane().add(new selectP3(main));
+	                // 추가: SelectP3 패널을 화면에 추가
+	                setVisible(false);
+	                main.getContentPane().add(new selectP3(main, category, 2)); // 2는 면 메뉴를 나타냅니다.
 	                main.revalidate();
 	                main.repaint();
 	            }
 	        });
+
 	        
 	    }
  }
@@ -266,7 +279,7 @@ class selectP1 extends JPanel {
 	    ImageIcon btn1_2 = new ImageIcon("./Button_Image/btn3_2.png");
 	    ImageIcon btn1_2_1 = new ImageIcon("./Button_Image/btn3_2_1.png");
 	 
-	 public selectP3(toDay_main_gui main) {
+	 public selectP3(toDay_main_gui main, int category, int menuType) {
 	        this.main = main;
 	        setLayout(null);
 	         
@@ -293,7 +306,7 @@ class selectP1 extends JPanel {
 	            public void actionPerformed(ActionEvent e) {
 	                // 추가: SelectP1 패널을 화면에 추가
 	            	setVisible(false);
-	                main.getContentPane().add(new result(main));
+	                main.getContentPane().add(new result(main, category, menuType, 0));
 	                main.revalidate();
 	                main.repaint();
 	            }
@@ -303,22 +316,209 @@ class selectP1 extends JPanel {
 	            public void actionPerformed(ActionEvent e) {
 	                // 추가: SelectP1 패널을 화면에 추가
 	            	setVisible(false);
-	                main.getContentPane().add(new result(main));
+	                main.getContentPane().add(new result(main, category, menuType, 1));
 	                main.revalidate();
 	                main.repaint();
 	            }
 	        });
+
 	        
 	    }
 
  }
- class result extends JPanel{
-	 private toDay_main_gui main;
-	 private JButton add;
-	 
-	 public result(toDay_main_gui main) {
-		 
-	 }
-}
+ class result extends JPanel {
+	    private toDay_main_gui main;
+	    private JButton add;
+	    private JLabel resultLabel;
+
+	    public result(toDay_main_gui main, int category, int menuType, int temperature) {
+	        this.main = main;
+	        setLayout(null);
+
+
+	        resultLabel = new JLabel("추천 메뉴: " + getRecommendedMenu(main, category, menuType, temperature));
+	        resultLabel.setSize(1000, 50);
+	        resultLabel.setLocation(100, 150);
+	        add(resultLabel);
+	    }
+
+	    private String getRecommendedMenu(toDay_main_gui main, int category, int menuType, int temperature) {
+	        Random random = new Random();
+	        String recommendedMenu = "국밥";
+	        
+	        
+	        String[] koreanRiceHotMenu = {"국밥", "김치찌개", "된장찌개"};
+	        String[] koreanRiceCoolMenu = {"비빔밥", "찬물에 밥"};
+	        String[] koreanBreadHotMenu = {"꽈배기", "찹쌀도넛", "찐빵", "떡볶이"};
+	        String[] koreanBreadCoolMenu = {"호두파이"};
+	        String[] koreanNoodleHotMenu = {"잔치국수", "칼국수"};
+	        String[] koreanNoodleCoolMenu = {"물냉면", "비빔냉면"};
+	        
+	        String[] westernRiceHotMenu = {"리소토", "크림 스프"};
+	        String[] westernRiceCoolMenu = {"리소토"};
+	        String[] westernBreadHotMenu = {"피자, 햄버거"};
+	        String[] westernBreadCoolMenu = {"파이", "햄버거"};
+	        String[] westernNoodleHotMenu = {"스파게티", "라자냐"};
+	        String[] westernNoodleCoolMenu = {"파스타 샐러드"};
+	        
+	        String[] ChineseRiceHotMenu = {"마파두부 덮밥", "짬뽕밥", "짜장밥", "마라탕"};
+	        String[] ChineseRiceCoolMenu = {"볶음밥"};
+	        String[] ChineseBreadHotMenu = {"만두", "꽃빵"};
+	        String[] ChineseBreadCoolMenu = {"만두", "꽃빵"};
+	        String[] ChineseNoodleHotMenu = {"짜장면", "짬뽕", "울면"};
+	        String[] ChineseNoodleCoolMenu = {"중국 냉면", "냉짬뽕"};
+
+	        String[] JapaneseRiceHotMenu = {"오야코동", "규동", "돈까스", "오므라이스"};
+	        String[] JapaneseRiceCoolMenu = {"초밥"};
+	        String[] JapaneseBreadHotMenu = {"타코야끼", "오코노미야끼"};
+	        String[] JapaneseBreadCoolMenu = {"타코야끼", "당고", "만쥬"};
+	        String[] JapaneseNoodleHotMenu = {"우동", "온소바", "라멘"};
+	        String[] JapaneseNoodleCoolMenu = {"냉소바"};
+
+
+	       if(category == 0) {
+	                if (menuType == 0) {
+	                	// 밥 메뉴
+	                    if (temperature == 0) {
+	                    	// 뜨거움
+		                    recommendedMenu = koreanRiceHotMenu[random.nextInt(koreanRiceHotMenu.length)];
+	                    }
+	                    else if(temperature == 1) {
+	                    	// 차가움
+		                    recommendedMenu = koreanRiceCoolMenu[random.nextInt(koreanRiceCoolMenu.length)];
+	                    }
+	                } 
+	                else if (menuType == 1) {
+	                    // 빵 메뉴
+	                    if ( temperature == 0) {
+	                    	// 뜨거움
+		                    recommendedMenu = koreanBreadHotMenu[random.nextInt(koreanBreadHotMenu.length)];
+	                    }
+	                    else if( temperature == 1) {
+	                    	// 차가움
+		                    recommendedMenu = koreanBreadCoolMenu[random.nextInt(koreanBreadCoolMenu.length)];
+	                    }
+	                }
+	                else if ( menuType == 2) {
+	                    // 면 메뉴
+	                    if ( temperature == 0) {
+	                    	// 뜨거움
+		                    recommendedMenu = koreanNoodleHotMenu[random.nextInt(koreanNoodleHotMenu.length)];
+	                    }
+	                    else if( temperature == 1) {
+	                    	// 차가움
+		                    recommendedMenu = koreanNoodleCoolMenu[random.nextInt(koreanNoodleCoolMenu.length)];
+	                    }
+	                }
+	       }
+	       else if( category==1) {
+	                if ( menuType == 0) {
+	                    // 밥 메뉴
+	                    if ( temperature == 0) {
+	                    	// 뜨거움
+		                    recommendedMenu = westernRiceHotMenu[random.nextInt(westernRiceHotMenu.length)];
+	                    }
+	                    else if( temperature == 1) {
+	                    	// 차가움
+		                    recommendedMenu = westernRiceCoolMenu[random.nextInt(westernRiceCoolMenu.length)];
+	                    }
+	                } 
+	                else if ( menuType == 1) {
+	                    // 빵 메뉴
+	                    if ( temperature == 0) {
+	                    	// 뜨거움
+		                    recommendedMenu = westernBreadHotMenu[random.nextInt(westernBreadHotMenu.length)];
+	                    }
+	                    else if( temperature == 1) {
+	                    	// 차가움
+		                    recommendedMenu = westernBreadCoolMenu[random.nextInt(westernBreadCoolMenu.length)];
+	                    }
+	                }
+	                else if ( menuType == 2) {
+	                    // 면 메뉴
+	                    if ( temperature == 0) {
+	                    	// 뜨거움
+		                    recommendedMenu = westernNoodleHotMenu[random.nextInt(westernNoodleHotMenu.length)];
+	                    }
+	                    else if( temperature == 1) {
+	                    	// 차가움
+		                    recommendedMenu = westernNoodleCoolMenu[random.nextInt(westernNoodleCoolMenu.length)];
+	                    }
+	                }
+	       }
+	       else if( category == 2) {
+	            	if ( menuType == 0) {
+	                    // 밥 메뉴
+	                    if ( temperature == 0) {
+	                    	// 뜨거움
+		                    recommendedMenu = ChineseRiceHotMenu[random.nextInt(ChineseRiceHotMenu.length)];
+	                    }
+	                    else if( temperature == 1) {
+	                    	// 차가움
+		                    recommendedMenu = ChineseRiceCoolMenu[random.nextInt(ChineseRiceCoolMenu.length)];
+	                    }
+	                } 
+	                else if ( menuType == 1) {
+	                    // 빵 메뉴
+	                    if ( temperature == 0) {
+	                    	// 뜨거움
+		                    recommendedMenu = ChineseBreadHotMenu[random.nextInt(ChineseBreadHotMenu.length)];
+	                    }
+	                    else if( temperature == 1) {
+	                    	// 차가움
+		                    recommendedMenu = ChineseBreadCoolMenu[random.nextInt(ChineseBreadCoolMenu.length)];
+	                    }
+	                }
+	                else if ( menuType == 2) {
+	                    // 면 메뉴
+	                    if ( temperature == 0) {
+	                    	// 뜨거움
+		                    recommendedMenu = ChineseNoodleHotMenu[random.nextInt(ChineseNoodleHotMenu.length)];
+	                    }
+	                    else if( temperature == 1) {
+	                    	// 차가움
+		                    recommendedMenu = ChineseNoodleCoolMenu[random.nextInt(ChineseNoodleCoolMenu.length)];
+	                    }
+	                }
+	       }
+	       else if( category == 3) {
+	                if ( menuType == 0) {
+	                    // 밥 메뉴
+	                    if ( temperature == 0) {
+	                    	// 뜨거움
+		                    recommendedMenu = JapaneseRiceHotMenu[random.nextInt(JapaneseRiceHotMenu.length)];
+	                    }
+	                    else if( temperature == 1) {
+	                    	// 차가움
+		                    recommendedMenu = JapaneseRiceCoolMenu[random.nextInt(JapaneseRiceCoolMenu.length)];
+	                    }
+	                } 
+	                else if ( menuType == 1) {
+	                    // 빵 메뉴
+	                    if ( temperature == 0) {
+	                    	// 뜨거움
+		                    recommendedMenu = JapaneseBreadHotMenu[random.nextInt(JapaneseBreadHotMenu.length)];
+	                    }
+	                    else if( temperature == 1) {
+	                    	// 차가움
+		                    recommendedMenu = JapaneseBreadCoolMenu[random.nextInt(JapaneseBreadCoolMenu.length)];
+	                    }
+	                }
+	                else if ( menuType == 2) {
+	                    // 면 메뉴
+	                    if ( temperature == 0) {
+	                    	// 뜨거움
+		                    recommendedMenu = JapaneseNoodleHotMenu[random.nextInt(JapaneseNoodleHotMenu.length)];
+	                    }
+	                    else if( temperature == 1) {
+	                    	// 차가움
+		                    recommendedMenu = JapaneseNoodleCoolMenu[random.nextInt(JapaneseNoodleCoolMenu.length)];
+	                    }
+	                } 
+	       }
+
+	        return recommendedMenu;
+	    }
+	}
  
  
